0a656253731fc2024219a0843f83e5a6bc5e34c7
yinqingnan <754417575@qq.com>
2020-08-19 18:01:06 +0800

    yqn

--- src/App.vue
+++ src/App.vue
@@ -29,5 +29,14 @@ export default {
 * {
   margin: 0;
   padding: 0;
+  -moz-user-select: none;
+
+  -webkit-user-select: none; /*webkit浏览器*/
+
+  -ms-user-select: none; /*IE10*/
+
+  -khtml-user-select: none; /*早期浏览器*/
+
+  user-select: none;
 }
 </style>
